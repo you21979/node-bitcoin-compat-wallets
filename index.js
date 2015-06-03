e449e8da2870d1c6411d7f5b98123ba54c5824c9
you21979 <you2197901@gmail.com>
2015-06-03 12:15:35 +0900

    mod

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.hdwallet = require('./lib/hdwallet');
 exports.hdwalletRo = require('./lib/hdwallet_ro');
 exports.bitcoinj = require('./lib/bitcoinj');
+exports.bitcore = require('bitcore');
