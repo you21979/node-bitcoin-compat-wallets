e9887218c769466bf8acc619cf85fb77a6dc7721
you21979 <you2197901@gmail.com>
2015-06-03 14:48:29 +0900

    mod

--- example/random.js
+++ example/random.js
@@ -1,11 +1,15 @@
 var wallets = require('..');
 var hdwallet = wallets.hdwallet;
-var bitcoinj = wallets.bitcoinj;
+var j = wallets.bitcoinj;
 var w1 = hdwallet.fromMnemonic("", "testnet");
 console.log(w1.getMasterPrivateKey())
+console.log(w1.getDerivedPrivateKey(j.derive_m_0h()))
 console.log(w1.getMasterPublicKey())
-console.log(w1.getBitcoinKeyPair(function(key){return bitcoinj.derive(key,0,0)}))
-console.log(w1.getBitcoinKeyPair(function(key){return bitcoinj.derive(key,0,1)}))
-console.log(w1.getBitcoinKeyPair(function(key){return bitcoinj.derive(key,0,2)}))
-console.log(w1.getBitcoinKeyPair(function(key){return bitcoinj.derive(key,0,3)}))
+console.log(w1.getDerivedPublicKey(j.derive_m_0h()))
+var i = 0;
+console.log(w1.getBitcoinKeyPair(j.derive_m_0h_x_x(0,i++)))
+console.log(w1.getBitcoinKeyPair(j.derive_m_0h_x_x(0,i++)))
+console.log(w1.getBitcoinKeyPair(j.derive_m_0h_x_x(0,i++)))
+console.log(w1.getBitcoinKeyPair(j.derive_m_0h_x_x(0,i++)))
+console.log(w1.getBitcoinKeyPair(j.derive_m_0h_x_x(0,i++)))
 
