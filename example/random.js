79583e69871b650776f95539b8d471c287a9b3d8
you21979 <you2197901@gmail.com>
2015-06-03 12:17:58 +0900

    mod

--- example/random.js
+++ example/random.js
@@ -2,6 +2,8 @@ var wallets = require('..');
 var hdwallet = wallets.hdwallet;
 var bitcoinj = wallets.bitcoinj;
 var w1 = hdwallet.fromMnemonic("", "testnet");
+console.log(w1.getMasterPrivateKey())
+console.log(w1.getMasterPublicKey())
 console.log(w1.getBitcoinKeyPair(function(key){return bitcoinj.derive(key,0,0)}))
 console.log(w1.getBitcoinKeyPair(function(key){return bitcoinj.derive(key,0,1)}))
 console.log(w1.getBitcoinKeyPair(function(key){return bitcoinj.derive(key,0,2)}))
