2633a43be9513373c5430c3790b0b75bb3d51e60
you21979 <you2197901@gmail.com>
2015-06-03 15:00:38 +0900

    add bip44

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
+exports.bitcore = require('bitcore');
 exports.hdwallet = require('./lib/hdwallet');
 exports.hdwalletRo = require('./lib/hdwallet_ro');
 exports.bitcoinj = require('./lib/bitcoinj');
-exports.bitcore = require('bitcore');
+exports.bip44 = require('./lib/bip44');
