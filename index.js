799377749cc65585dac6f73b21b7734cd969d130
you2 <you2197901@gmail.com>
2015-10-27 01:46:17 +0900

    update bitcore-lib

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-exports.bitcore = require('bitcore');
+exports.bitcore = require('bitcore-lib');
 exports.hdwallet = require('./lib/hdwallet');
 exports.hdwalletRo = require('./lib/hdwallet_ro');
 exports.bitcoinj = require('./lib/bitcoinj');
